2616b44ec77ba8f85d2c8613b1bbd5c1cc6df8dd
Juan Pablo Blariza <juanblariza@gmail.com>
2024-09-24 18:07:31 -0300

    feature(): add changes to test talk

--- src/components/BusinessHoursDay.vue
+++ src/components/BusinessHoursDay.vue
@@ -143,11 +143,6 @@ export default {
     ToggleButton,
     FontAwesomeIcon
   },
-  data() {
-    return {
-      isDisabled: false
-    };
-  },
   mixins: [helperMixin, validationMixin],
   props: {
     day: {
@@ -195,24 +190,22 @@ export default {
       return this.hours.some(hour => {
         return hour.isOpen === true;
       });
+    },
+    isDisabled: {
+      get: function () {
+        return this.hours[0].isOpen &&  this.hours[0].open === '24hrs'
+      },
+      set: function (newValue) {
+          // var names = newValue.split(' ')
+          // this.firstName = names[0]
+          // this.lastName = names[names.length - 1]
+      }
     }
   },
   directives: {
     visible: function(el, binding) {
       el.style.visibility = binding.value ? 'visible' : 'hidden';
     }
-  },
-  mounted: function() {
-    this.isDisabled = true
-    
-    this.hours.forEach((day, index) => {
-        if (day.isOpen && day.open === '24hrs') {
-           this.hours[index].close = ''
-           this.isDisabled = true
-        }
-    });
-
-    this.runValidations();
   },
   methods: {
     onChangeEventHandler: function(whichTime, index, value) {
